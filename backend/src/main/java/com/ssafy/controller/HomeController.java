71f2a6a85d73ba8561fc90485301c1bfb56b10e9
Ricky <gwtf1210@naver.com>
2019-08-12 18:51:49 +0900

    최종 커밋

--- backend/src/main/java/com/ssafy/controller/HomeController.java
+++ backend/src/main/java/com/ssafy/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.ssafy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -10,5 +12,4 @@ public class HomeController {
 	public String index() {
 		return "index";
 	}
-	
 }
