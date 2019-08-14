d81749d11d6a4955ad35754a00c5f243f988b0c7
최재형 <jae637@naver.com>
2019-08-14 10:28:24 +0900

    푸시 전

--- frontend/src/components/home/Introduction.vue
+++ frontend/src/components/home/Introduction.vue
@@ -5,12 +5,16 @@
 		<!-- Description -->
 		<div class="row mt-3">
 			<div class="col-6" style="height:300px;">
-				<h1>Here We are</h1>
-				<p> There's nothing to say specially </p>
+				<h1>Here We are</h1> <br>
+				<p> 정경일 : FullStack G.O.D </p>
+				<p> 이기문 : Backend One Top </p>
+				<p> 박수빈 : Already AhnLab Empolyee </p>
+				<p> 강성진 : Team Reader </p>
+				<p> 최재형 : Pro Deploier </p>
 			</div>
 			<div class="col-6 mr-0 mt-3">
-				<ImageOverlay title="Title"
-			                body="What is all of it for?"
+				<ImageOverlay title="팀원들"
+			                body="Order by Left"
 			                imgSrc="https://i.imgur.com/qvyCoLr.png">            
 			  </ImageOverlay>
 			</div>
