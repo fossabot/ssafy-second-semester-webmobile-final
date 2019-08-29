26a7db0a2ed8ff1618f55c79f94ab82ccfe8d19e
ksj0206 <ksj0206@uos.ac.kr>
2019-08-29 09:02:15 +0900

    주석 정리

--- frontend/src/apis/mainservice/mainServices.js
+++ frontend/src/apis/mainservice/mainServices.js
@@ -18,7 +18,6 @@ const portfolioUrl = 'https://70.12.246.106:9090/api/bears/portfolios'
 const postUrl = 'https://70.12.246.106:9090/api/bears/posts' 
 
 export default {
-  // 이 함수는 근데 이미 account.js에 변수화 되어 있어서 vue 특성상 변수화 된 애를 쓰는게 편함 버리는 함수
   isLogin() {
     if (loginUser.loginCheck) {
       return true
@@ -27,8 +26,7 @@ export default {
     }
   },
 
-  // 근데 얘네도 걍 state 에서 바로 접근하는게 나을듯 아니지 얘는 근데 인자가 있네...
-  isAuthor(authorEmail) { // 이건 프론트에서 버튼 보이게 할지 말지 
+  isAuthor(authorEmail) { 
     if (loginUser.accountEmail === authorEmail) {
       return true
     } else {
@@ -36,16 +34,15 @@ export default {
     }
   },
 
-  // 얘는 스테이트 직접접근이 나을듯
-  isTeam() { // 나중에 글 작성할 때 필요한 함수
-    if (loginUser.accountAuth === 3) { // 방문자
+  isTeam() { 
+    if (loginUser.accountAuth === 3) { 
       return false
     } else {
       return true
     }
   },
 
-  isAdmin() { // 얘도
+  isAdmin() {
     if (loginUser.accountAuth === 1) {
       return true
     } else {
@@ -68,7 +65,7 @@ export default {
                 })
   },
 
-  async loadPortfolios(pageNo) { // 6개씩
+  async loadPortfolios(pageNo) {
     const headers = {
       "Content-Type": "application/json",
       accountEmail: "",
@@ -110,8 +107,6 @@ export default {
   },
 
   postPortfolio(portfolio) { 
-    // 새 글 작성은 작성자 체크 할 필요 없이 바로 현재 로그인 유저로 작성하면 된다. 애초에 로그인한 유저에게만 포스트 버튼이 보이므로
-    // but 만일을 위해 여기도 로그인 안했을시를 분기해서 에러 핸들링하자.. 나중에^^
     const headers = {
       "Content-Type": "application/json",
       "accountEmail": loginUser.accountEmail,
@@ -154,8 +149,6 @@ export default {
   // Portfolio Comment CRUD
   
   postPortfolioComment(portfolioId, portfolioCommentContent) {
-    // 새 댓글 작성은 작성자 체크 할 필요 없이 바로 현재 로그인 유저로 작성하면 된다. 애초에 로그인한 유저에게만 포스트 버튼이 보이므로
-    // but 만일을 위해 여기도 로그인 안했을시를 분기해서 에러 핸들링하자.. 나중에^^
     const portfolioComment = {
       "portfolioId": portfolioId,
       "accountEmail": loginUser.accountEmail,
@@ -173,7 +166,6 @@ export default {
                 })
   }, 
   
-  // 어차피 프론트 단에서 작성자 식별 및 로그인 식별하고 요청이 실행되므로 여기서 권한 담아서 보낸다.
   putPortfolioComment(portfolioId, portfolioComment) {
     const headers = {
       "Content-Type": "application/json",
@@ -186,7 +178,6 @@ export default {
                 })
   },
 
-  // 어차피 프론트 단에서 작성자 식별 및 로그인 식별하고 요청이 실행되므로 여기서 권한 담아서 보낸다.
   deletePortfolioComment(portfolioId, portfolioCommentId) {
     const headers = {
       "Content-Type": "application/json",
@@ -216,7 +207,7 @@ export default {
                 })
   },
   
-  async loadPosts(pageNo) { // 6개씩
+  async loadPosts(pageNo) { 
     const headers = {
       "Content-Type": "application/json",
       accountEmail: "",
@@ -257,8 +248,6 @@ export default {
   },
 
   postPost(post) { 
-    // 새 글 작성은 작성자 체크 할 필요 없이 바로 현재 로그인 유저로 작성하면 된다. 애초에 로그인한 유저에게만 포스트 버튼이 보이므로
-    // but 만일을 위해 여기도 로그인 안했을시를 분기해서 에러 핸들링하자.. 나중에^^
     const headers = {
       "Content-Type": "application/json",
       "accountEmail": loginUser.accountEmail,
@@ -343,6 +332,7 @@ export default {
       return res.data
     })
   },
+ 
   // 기타
   parsePortfolio(portfolioContent) {
     let contents_start = []
